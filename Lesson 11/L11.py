e13b2efdd76a24529711764d124655bdd64ab784
alexol08 <aologhlen08@gmail.com>
2025-07-24 19:02:46 +0100

    Lesson 12 update

--- Lesson 11/L11.py
+++ Lesson 11/L11.py
@@ -1,14 +1,42 @@
-with open("homework.txt", 'w') as file:
-    file.write("Do algebra problems \nEnglish essay on clouds \nLearn 10 new words of French")
+# with open("homework.txt", 'w') as file:
+#     file.write("Do algebra problems \nEnglish essay on clouds \nLearn 10 new words of French")
 
-with open("homework.txt", 'a') as file:
-    file.write("\nGive permission slip")
+# with open("homework.txt", 'a') as file:
+#     file.write("\nGive permission slip")
 
-with open("homework.txt", 'r') as file:
-    hw=file.read()
-    print(hw)
+# with open("homework.txt", 'r') as file:
+#     hw=file.read()
+#     print(hw)
+import pgzrun
 
-## You can use readlines and readline in the read function
+WIDTH=500
+HEIGHT=500
+
+def readfile():
+    QUESTIONS=[]
+    with open("questions.txt", 'r') as file:
+        data=file.readlines()
+
+        for line in data:
+            x=line.split(",")
+            quest_dict={
+                "question": x[0],
+                "options": x[1:5], 
+                "answer":x[-1]
+            }
+            print(quest_dict)
+            QUESTIONS.append(quest_dict)
+    print(QUESTIONS)
+
+quest_box=Rect((50,), (200, 100))
+
+
+def draw():
+    screen.draw.filled_rect(quest_box, color = (255, 255, 255))
+
+
+
+pgzrun.go()
 
 # git init
 # git add .
